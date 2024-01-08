4690926ec10c73321c7a4a39b1de09febc4c815a
nisrinawafaz <nisrinawafaz@gmail.com>
2024-01-08 23:47:23 +0700

    fixing script MUI dan styling

--- react-resep-makanan/src/pages/Login.jsx
+++ react-resep-makanan/src/pages/Login.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Box from '@mui/material/Box';
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import AuthWrapper from "../components/AuthWrapper";
 import {BlueButton} from "../components/Button"
 import { Logo } from '../components/Logo';
-import { TextInput, PasswordInput } from "../components/TextField";
+import { 
+  TextInput, 
+  PasswordInput
+} from "../components/TextField";
+import { 
+  wrapper,
+  formContentWrapper
+} from '../styles/style.jsx';
 
 const cssReset = `
   * {
@@ -54,63 +62,8 @@ const LoginPage = () => {
         console.log(data);
       };
     
-      const wrapper = {
-        fontFamily: 'Mulish, sans-serif',
-        margin: "0", // m-0
-        display: "flex", // flex
-        flexDirection: "column", // flex-col
-        alignItems: "center", // items-center
-        marginRight: "auto", // mx-auto
-        marginLeft: "auto",
-        marginBottom:"auto",
-        marginTop:"auto",
-        paddingRight: "1rem", // px-4
-        paddingLeft: "1rem",
-        gap: "0.5rem", // gap-2
-        paddingTop: "2.5rem", // py-10
-        paddingBottom: "2.5rem",
-      };
-    
-      const titleWrapper = {
-        display: "flex", // flex
-        flexDirection: "column", // flex-col
-        alignItems: "center", // items-center
-        marginBottom: "1rem"
-      };
-    
-      const titleStyle = {
-        fontSize: "1.125rem", // text-lg
-        lineHeight: "1.75rem",
-        fontWeight: "700" // font-bold
-      }
-    
-      const formContentWrapper = {
-        backgroundColor: "white",
-        paddingLeft: "2rem",
-        paddingRight: "2rem",
-        paddingTop: "1.5rem",
-        paddingBottom: "1.5rem",
-        display: "flex",
-        flexDirection: "column",
-        gap: "1rem",
-        alignItems: "center",
-      }
-    
-      const buttonStyle = {
-        backgroundColor: "#01bfbf",
-        color: "white",
-        border: "transparent",
-        width: "100%",
-        paddingTop: "0.5rem",
-        paddingBottom: "0.5rem",
-        borderRadius: "calc(0.5rem - 4px)",
-        "&:hover": {
-          backgroundColor: "#01acac",
-        },
-      }
-
   return (
-    <div style={wrapper} >
+    <Box sx={wrapper} >
       <style>{cssReset}</style>
       <Logo />
       <AuthWrapper
@@ -120,7 +73,6 @@ const LoginPage = () => {
         footerText="Belum punya Akun? "
         showAboutAndContact={true}
       >
-        
         <form onSubmit={handleSubmit(onSubmit)} style={formContentWrapper}>
         <TextInput 
             label="Username"
@@ -137,7 +89,8 @@ const LoginPage = () => {
           <BlueButton text="Login" customStyle={{width: '100%'}} />
         </form>
       </AuthWrapper>
-    </div>
+      
+    </Box>
   );
 };
 
